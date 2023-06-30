4bcb3ee1847cc3851dc66f2b8cd66896de058fad
Felipe Soares <felipe.11.11@hotmail.com>
2023-06-30 15:52:17 -0400

    Include password generator

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const {password, setPassword} = useState()
+  const [password, setPassword] = useState()
+
+  function passwordGen() {
+
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = 12;
+    let newPassword = ""
+
+    for (let i = 0; i <= passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * chars.length);
+      newPassword += chars.substring(randomNumber, randomNumber +1);
+     }
+     
+    setPassword(newPassword)
+
+  }
+
   return (
     <>
       <h1>Gerador de Senhas</h1>
       <div className='buttons'>
       <button
-        onClick={()=>setPassword(Math.floor(Math.random()))}
+        onClick={()=>passwordGen()}
       >
         Gerar
       </button>
